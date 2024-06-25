95d16221970ec28787e2927fb0f89b0be73eafde
IOENKIS <90310655+IOENKIS@users.noreply.github.com>
2024-06-25 11:15:03 +0300

    Initial Commit

---
+++ PrinterApp/PrinterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterAppApp.swift
+//  PrinterApp
+//
+//  Created by IVANKIS on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrinterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
